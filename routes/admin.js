4c4f21e170e5d00a785f702209b942a3da9af01a
Leonardo Tapani <93481468+LeonardoTrapani@users.noreply.github.com>
2022-06-17 18:15:18 +0200

    basic routing with html views

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,12 @@
 const express = require("express");
+const path = require("path");
 
 const router = express.Router();
 
 //  admin/add-product
 router.get("/add-product", (req, res, next) => {
   console.log("in another the middleware!");
-  res.send(
-    '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">AddProduct</button></form>'
-  );
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res, next) => {
