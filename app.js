106fd920569135250179bfd1d4d4f7e0a0722fa1
Leonardo Tapani <93481468+LeonardoTrapani@users.noreply.github.com>
2022-06-17 19:13:10 +0200

    public folder

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const rootDir = require("./util/path");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(rootDir, "public")));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res) => {
-  res.status(400).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(400).sendFile(path.join(rootDir, "views", "404.html"));
 });
 
 app.listen(3001);
