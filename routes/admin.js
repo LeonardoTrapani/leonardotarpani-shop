f35fd3fd5dc0cda9f3225ad9a813b61acea43c0c
Leonardo Tapani <93481468+LeonardoTrapani@users.noreply.github.com>
2022-06-18 14:36:26 +0200

    cleaning project + ejs implementation

--- routes/admin.js
+++ routes/admin.js
@@ -13,8 +13,6 @@ router.get("/add-product", (req, res, next) => {
   res.render("add-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
-    formsCss: true,
-    activeAddProduct: true,
   });
 });
 
