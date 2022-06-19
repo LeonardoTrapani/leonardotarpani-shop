0e80c166b324ecf800da94d6e63f2ac4ac3c5bec
Leonardo Tapani <93481468+LeonardoTrapani@users.noreply.github.com>
2022-06-19 17:56:05 +0200

    remove from cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -37,15 +37,17 @@ exports.getCart = (req, res, next) => {
     Product.fetchAll((products) => {
       const cartProducts = [];
       for (product of products) {
-        const cartProductData = cart.products.find(prod => prod.id === product.id);
+        const cartProductData = cart.products.find(
+          (prod) => prod.id === product.id
+        );
         if (cartProductData) {
-          cartProducts.push({productData: product, qty: cartProductData.qty});
+          cartProducts.push({ productData: product, qty: cartProductData.qty });
         }
       }
       res.render("shop/cart", {
         path: "/cart",
         pageTitle: "Your Cart",
-        products: cartProducts
+        products: cartProducts,
       });
     });
   });
@@ -57,6 +59,14 @@ exports.postCart = (req, res, next) => {
     Cart.addProduct(prodId, product.price);
   });
   res.redirect("/cart");
+};
+
+exports.postCartDeleteItem = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, (product) => {
+    Cart.deleteProduct(prodId, product.price);
+    res.redirect("/cart");
+  });
 };
 
 exports.getOrders = (req, res, next) => {
--- models/cart.js
+++ models/cart.js
@@ -50,7 +50,7 @@ module.exports = class Cart {
       updatedCart.products = updatedCart.products.filter(
         (prod) => prod.id !== id
       );
-      updatedCart.totalPrice = cart.totalPrice - productPrice * productQty;
+      updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQty;
       fs.writeFile(p, JSON.stringify(updatedCart), (err) => {
         console.log(err);
       });
