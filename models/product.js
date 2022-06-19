1ec725d65af5e388f05d89ccbdde01bb0101568c
Leonardo Tapani <93481468+LeonardoTrapani@users.noreply.github.com>
2022-06-19 15:55:38 +0200

    add to cart

--- models/product.js
+++ models/product.js
@@ -1,13 +1,13 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
 const p = path.join(
   path.dirname(process.mainModule.filename),
-  "data",
-  "products.json"
+  'data',
+  'products.json'
 );
 
-const getProductsFromFile = (cb) => {
+const getProductsFromFile = cb => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
       cb([]);
@@ -27,9 +27,9 @@ module.exports = class Product {
 
   save() {
     this.id = Math.random().toString();
-    getProductsFromFile((products) => {
+    getProductsFromFile(products => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
+      fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
       });
     });
@@ -40,8 +40,8 @@ module.exports = class Product {
   }
 
   static findById(id, cb) {
-    getProductsFromFile((products) => {
-      const product = products.find((p) => p.id === id);
+    getProductsFromFile(products => {
+      const product = products.find(p => p.id === id);
       cb(product);
     });
   }
