af618e34b663d769587b52c06f081a254ab45df4
Leonardo Tapani <93481468+LeonardoTrapani@users.noreply.github.com>
2022-06-23 19:44:44 +0200

    first mongodb commit

--- util/database.js
+++ util/database.js
@@ -1,13 +1,29 @@
-const Sequelize = require("sequelize");
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize(
-  "leonardotrapani-shop",
-  "root",
-  "miPiaceComputer37",
-  {
-    dialect: "mysql",
-    host: "localhost",
+let _db;
+
+const mongoConnect = callback => {
+  MongoClient.connect(
+    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
+  )
+    .then(client => {
+      console.log('Connected!');
+      _db = client.db();
+      callback();
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
   }
-);
+  throw 'No database found!';
+};
 
-module.exports = sequelize;
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
