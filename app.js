f0253971019d7614596a6b3760521fb0cb619807
Leonardo Tapani <93481468+LeonardoTrapani@users.noreply.github.com>
2022-06-23 22:04:05 +0200

    mongoose

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const mongoose = require('mongoose');
+
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
 const app = express();
@@ -20,11 +21,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
   User.findById('5baa2528563f16379fc8a610')
-    .then(user => {
+    .then((user) => {
       req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
@@ -32,6 +33,9 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose
+  .connect(
+    'mongodb+srv://node-app:kuFAEHub8bBEJLh9@cluster0.v1gv410.mongodb.net/?retryWrites=true&w=majority '
+  )
+  .then((result) => app.listen(6969))
+  .catch((error) => console.log(error));
