a28ba1e8b7d376fc1dcf819c012e964cad8b7c05
Leonardo Tapani <93481468+LeonardoTrapani@users.noreply.github.com>
2022-06-28 12:34:12 +0200

    page 500 error, initial error handling

--- controllers/admin.js
+++ controllers/admin.js
@@ -56,7 +56,9 @@ exports.postAddProduct = (req, res, next) => {
       res.redirect('/admin/products');
     })
     .catch((err) => {
-      console.log(err);
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
     });
 };
 
